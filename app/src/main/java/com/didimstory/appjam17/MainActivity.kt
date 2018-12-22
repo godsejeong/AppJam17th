b070ca16bff4de748322453e7e840e1cafd33a51
godsejeong <dbdpxm@naver.com>
2018-12-22 17:49:54 +0900

    bottomnavigationbar Commit

--- app/src/main/java/com/didimstory/appjam17/MainActivity.kt
+++ app/src/main/java/com/didimstory/appjam17/MainActivity.kt
@@ -3,32 +3,79 @@ package com.didimstory.appjam17
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.ActivityCompat
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
 import android.view.MenuItem
+import android.widget.FrameLayout
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var fragment : Fragment
+    lateinit var fragmentManager: FragmentManager
+
+    private val FINSH_INTERVAL_TIME = 2000
+    private var backPressedTime:Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.action_main -> {
-//                        fragment = SeasonFragment.newInstance()
-                }
-                R.id.action_cody -> {
-//                        fragment = CodyFragment.newInstance()
-                }
-                R.id.action_sale -> {
-//                        fragment = SaleFragment.newInstance()
-                }
-                R.id.action_locker -> {
-//                        fragment = LockerFragment.newInstance()
-                }
+        fragmentManager = supportFragmentManager
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        fragment = ITFragment.newInstance()
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.content, fragment).commit()
+
+    }
+
+    val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.actionIT -> {
+
             }
-            val transaction = fragmentManager.beginTransaction()
-//                transaction.replace(R.id.content, fragment).commit()
-            true
+            R.id.actionSprots -> {
+
+            }
+
+            R.id.actionEducation -> {
+
+            }
+            R.id.actionSell -> {
+
+            }
+
+            R.id.actionShowpping -> {
+
+            }
+
+            R.id.actionCook -> {
+
+            }
+
+            R.id.actionLend -> {
+
+            }
+
+            R.id.actionAuther-> {
+
+            }
+        }
+        val transaction = fragmentManager.beginTransaction()
+                transaction.replace(R.id.content,fragment).commit()
+        true
+    }
+
+    override fun onBackPressed() {
+        var tempTime = System.currentTimeMillis()
+        var intervalTime = tempTime - backPressedTime
+        if (intervalTime in 0..FINSH_INTERVAL_TIME) {
+            ActivityCompat.finishAffinity(this)
+        } else {
+            backPressedTime = tempTime
+            Toast.makeText(applicationContext, "한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show()
         }
     }
 }
